40925267a0f55b1ca9f0dd3948b52b6df57bf1b9
LikiBlaze <LikiBlaze@foxmail.com>
2020-12-14 17:18:36 +0800

    Site updated: 2020-12-14 17:18:34

---
+++ css/new.js
@@ -0,0 +1,8 @@
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+  full_page[0].style.background = "transparent";
+}
+var xiaokang = new xkTool();
+xiaokang.footFish();
+xiaokang.appendSocial({
+});
