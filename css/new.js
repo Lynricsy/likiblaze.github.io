06f65d40d733dd20205f69dc07f9db9fa71b018f
LikiBlaze <likiblaze@foxmail.com>
2020-12-23 17:01:21 +0800

    Site updated: 2020-12-23 17:01:15

--- css/new.js
+++ css/new.js
@@ -2,7 +2,7 @@ var full_page = document.getElementsByClassName("full_page");
 if (full_page.length != 0) {
   full_page[0].style.background = "transparent";
 }
-var xiaokang = new xkTool();
+var xiaokang = new xkTool("transparent");
 xiaokang.footFish();
 xiaokang.appendSocial({
 });
