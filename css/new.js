f3b2cca9e3b52a9760359b6903fcf0218182ab4a
Thomitics <likiblaze@foxmail.com>
2021-02-01 07:40:49 +0800

    Site updated: 2021-02-01 07:40:47

--- css/new.js
+++ css/new.js
@@ -1,8 +1,1 @@
-var full_page = document.getElementsByClassName("full_page");
-if (full_page.length != 0) {
-  full_page[0].style.background = "transparent";
-}
-var xiaokang = new xkTool("transparent");
-xiaokang.footFish();
-xiaokang.appendSocial({
-});
+"use strict";var full_page=document.getElementsByClassName("full_page");0!=full_page.length&&(full_page[0].style.background="transparent");var xiaokang=new xkTool("transparent");xiaokang.footFish(),xiaokang.appendSocial({});
